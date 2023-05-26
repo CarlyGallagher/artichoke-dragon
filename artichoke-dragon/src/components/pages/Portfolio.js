fd12246afee30404d801c439260fc6527eb73800
CarlyGallagher <jagllaghercarly3@yahoo.com>
2023-05-26 13:54:38 -0700

    added project links for github

--- artichoke-dragon/src/components/pages/Portfolio.js
+++ artichoke-dragon/src/components/pages/Portfolio.js
@@ -3,7 +3,31 @@ import React from "react";
 export default function Portfolio() {
     return (
         <div>
-        <h3>My projects</h3>
+            <h3>My projects</h3>
+            <div>
+                <a href="https://carlygallagher.github.io/stone-martini/">
+                    <h3>Sip Savvy</h3>
+                </a>
+                <a href="https://github.com/CarlyGallagher/stone-martini.git">
+                    <h4>link to github repository</h4>
+                </a>
+            </div>
+            <div>
+                <a href="https://the-honey-comb.herokuapp.com/">
+                    <h3>Honey comb</h3>
+                    <a href="https://github.com/CarlyGallagher/the-honey-comb.git">
+                        <h4>link to github repository</h4>
+                    </a>
+                </a>
+            </div>
+            <div>
+                <a href="https://ramen-talks.herokuapp.com/login">
+                    <h3>Honey comb</h3>
+                    <a href="https://github.com/CarlyGallagher/ramen-talks.git">
+                        <h4>link to github repository</h4>
+                    </a>
+                </a>
+            </div>
         </div>
     );
 }
