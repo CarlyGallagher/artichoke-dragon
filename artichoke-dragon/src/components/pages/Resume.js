3106563ba781dedb487d4399679d6eca392d4124
CarlyGallagher <jagllaghercarly3@yahoo.com>
2023-05-25 19:18:11 -0700

    added resume download

--- artichoke-dragon/src/components/pages/Resume.js
+++ artichoke-dragon/src/components/pages/Resume.js
@@ -1,13 +1,13 @@
 import React from "react";
+import resumePDF from "../../assets/resume/CarlyGallagherResume.pdf";
 
 export default function Resume() {
     return (
         <div>
             <h3>Resume</h3>
             <button className="button">
-                <a className="button" href="../assets/resume/CarlygallagherResume.pdf" download="CarlyGallagherResume.pdf">
-                    Download Resume
-                </a>
+            <a className="button" href={resumePDF} download="CarlyGallagherResume.pdf"></a>
+                download Resume
             </button>
         </div>
     );
