95c92f23cf895e33541d8521028cb994f5bf4062
CarlyGallagher <jagllaghercarly3@yahoo.com>
2023-05-25 18:49:57 -0700

    aboutme photo, resume downloadable

--- artichoke-dragon/src/components/pages/Resume.js
+++ artichoke-dragon/src/components/pages/Resume.js
@@ -3,7 +3,12 @@ import React from "react";
 export default function Resume() {
     return (
         <div>
-        <h3>Resume</h3>
+            <h3>Resume</h3>
+            <button className="button">
+                <a className="button" href="../assets/resume/CarlygallagherResume.pdf" download="CarlyGallagherResume.pdf">
+                    Download Resume
+                </a>
+            </button>
         </div>
     );
 }
