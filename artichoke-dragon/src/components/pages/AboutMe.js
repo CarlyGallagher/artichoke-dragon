bba9778e55d08f4012acfeea827bf48c49aecffc
CarlyGallagher <jagllaghercarly3@yahoo.com>
2023-06-09 18:54:52 -0700

    fixed rows for portfolio

--- artichoke-dragon/src/components/pages/AboutMe.js
+++ artichoke-dragon/src/components/pages/AboutMe.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function AboutMe() {
   return (
     <div>
-  <img src={require("../../assets/profilePhoto.jpg")} alt="profile photo" />
+  <img className="profilephoto" src={require("../../assets/profilePhoto.jpg")} alt="profile photo" />
   <p className="aboutMe">
     Welcome to my page! I am a full stack web developer aiming to write the best applications possible!
     I have a nack for organization so data systems are my bread and butter!!! 🍞 🧈
